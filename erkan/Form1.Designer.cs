49e04527340b0eb1d1742175d59fedcc1b664d00
ekran123 <shikatesterkan@outlook.com>
2024-10-31 13:07:10 +0300

    Add files via upload

---
+++ erkan/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace erkan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::erkan.Properties.Resources.Yui_Hirasawa_new_mugshot;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(169, -6);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(68, 154);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::erkan.Properties.Resources.images;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(-14, 86);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(68, 154);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::erkan.Properties.Resources.download;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(243, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(68, 154);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::erkan.Properties.Resources.Noko;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(113, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 154);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "özel basma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 190);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "koshitan";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ erkan/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Media;
+using System.IO;
+
+
+namespace erkan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaySound()
+        {
+            // Gömülü kaynak adını belirtin
+            var resourceName = "erkan.music.wav";
+
+            using (Stream audioStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                if (audioStream != null)
+                {
+                    using (SoundPlayer player = new SoundPlayer(audioStream))
+                    {
+                        player.Play(); // Müzik çalmaya başla
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ses dosyası bulunamadı.");
+                }
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("kapıcanmı", "shikanoko", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            
+            if (result == DialogResult.No)
+            {
+                MessageBox.Show("(nun)\r\nShikanoko nokonoko koshi tantan\r\nShikanoko nokonoko koshi tantan\r\nShikanoko nokonoko koshi tantan\r\nShikanoko nokonoko koshi tantan\r\n(nun!)\r\n\r\nShikanoko tokotoko arawaru nokonoko\r\nTsuno tsuno pyoko pyoko mitame wa onya no ko?\r\nChikagoro uwasa no nakanaka kiwamono\r\nOtenami ikahodo henachoko?\r\n\r\nSugoku mukashi kara japan ni seisoku\r\nShika senbei nara mugen ni kudasai!\r\nTonakai wa nakama kamoshika wa ushika\r\nDemo demo matomete guuteimoku\r\n\r\nNun no suteppu de odorou\r\nYo ga akeru made (wan, tsuu, surii)\r\n\r\nNando datte hansuu shite nu nu nun\r\nYosoku fukanou shika iro deizu\r\nAbangyarudo de oorai kaosu mashi mashi↑↑\r\n\r\nItsu no ma ni kuse ni natte nu nu nun\r\nHacha de mecha na shika iro deizu\r\nHatenkou bacchi Come on!\r\nNandemo ari ruuru nante nashi\r\n\r\nKimi no egao de genki mantan\r\nMoshikashite unmei no ma, ma, My Deer!?\r\nKokoro no dokoka de koshi tantan\r\nKimi to nara itsu made mo\r\n\r\nShikanoko nokonoko koshi tantan\r\nShikanoko nokonoko koshi tantan\r\n\r\n[Full Version Continues]\r\n\r\n[thanks for visiting animesonglyrics.com]\r\n\r\nShikanoko shika katan! odaterya ichikoro?\r\nIya iya igai to tsuwamono kamo yo\r\nYaru koto nasu koto dotabata sawagi de\r\nTsugou ga warukerya terepooto!?\r\n\r\nYoukoso shikabu e atsumare shika tomo\r\nShika ni naritakerya kewashii michi da zo\r\nImadoki JK nara jidai wa shika desho\r\nShika senbei katate ni hai, poozu\r\n\r\nNun no koe ga hibiku yo\r\nChikyuu wo koete doko made mo\r\n\r\nKureejii sore tte hometeru?\r\nTorikkii de ii jan puchohenza\r\n\r\nKami no tsukaitaru shika wo agame yo\r\nShikanoko kami no ko banbanzai!\r\nKami no tsukaitaru shika wo agame yo\r\nShikanoko kami no ko banbanzai!\r\n\r\nSeishun tte yatsu kamo nante\r\nKirari kirari kagayaiteru ne\r\nMabushi sugiru nara gurasan hai (.∀.) つ dozoo\r\n\r\nNando datte hansuu shite nu nu nun\r\nYosoku fukanou shika iro deizu\r\nAbangyarudo de oorai kaosu mashi mashi↑↑\r\n\r\nItsu no ma ni kuse ni natte nu nu nun\r\nHacha de mecha na shika iro deizu\r\nHatenkou bacchi Come on!\r\nNandemo ari ruuru nante nashi\r\n\r\nKimi no egao de genki mantan\r\nMoshikashite unmei no ma, ma, My Deer!?\r\nKokoro no dokoka de koshi tantan\r\nJinrui shikaka keikaku\r\nSonna koto wa sasenai\r\n(demo!)\r\nKimi to nara itsu made mo\r\n\r");
+                e.Cancel = true; // Kapatmayı iptal et
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlaySound();
+        }
+    }
+}
